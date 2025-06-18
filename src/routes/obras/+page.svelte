0897d9708e52615f46e25acd6c9868890b107ff7
mara <mara@nekopath.fun>
2025-06-18 13:36:29 -0600

    changes to margin spacing on list of works

--- src/routes/obras/+page.svelte
+++ src/routes/obras/+page.svelte
@@ -10,9 +10,8 @@
         <li>
             <a href = "/obras/middletreatise">notes on the middle treatise</a> : una monografía acerca de la <i>mūlamadhyamakakārikā</i> de nagarjuna (en inglés). primero publicado: 05 abríl 2025. último revisto: 13 abríl 2025. 
         </li>
-
         <li>
-            <a href ="/obras/introtologic">tarski's introducción a la lógica</a> :mis soluciones para los ejercicios de "introducción a la lógica y a la metología de las ciencias deductivas" por tarski (en ingles). primera parte publicado: 18 junio 2025.
+            <a href ="/obras/introtologic">tarski's introducción a la lógica</a> : mis soluciones para los ejercicios de "introducción a la lógica y a la metología de las ciencias deductivas" por tarski (en ingles). primera parte publicado: 18 junio 2025.
         </li>
     </ul>
 </div>
@@ -24,5 +23,9 @@
 
     ul {
         list-style: none;
+    }
+
+    li {
+        margin-bottom: 1rem;
     }
 </style>
