e17b81b017755edc337ccb85af383f9e1656c8f7
mara <mara@nekopath.fun>
2025-06-18 13:28:58 -0600

    added tarski pt 1 exercises

--- src/routes/obras/+page.svelte
+++ src/routes/obras/+page.svelte
@@ -4,15 +4,17 @@
 <div>
     <br>
     <h3>
-        mis obras académicas
+        obras académicas
     </h3>
     <ul>
         <li>
             <a href = "/obras/middletreatise">notes on the middle treatise</a> : una monografía acerca de la <i>mūlamadhyamakakārikā</i> de nagarjuna (en inglés). primero publicado: 05 abríl 2025. último revisto: 13 abríl 2025. 
         </li>
-    </ul>
 
-    próximo: tareas de <i>introducción a la lógica y a la metología de las ciencias deductivas</i> de tarski.
+        <li>
+            <a href ="/obras/introtologic">tarski's introducción a la lógica</a> :mis soluciones para los ejercicios de "introducción a la lógica y a la metología de las ciencias deductivas" por tarski (en ingles). primera parte publicado: 18 junio 2025.
+        </li>
+    </ul>
 </div>
  
 <style>
---
+++ src/routes/obras/introtologic/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    import '../../../app.css'
+</script>
+
+<div class = "blog">
+    <h3>intro to logic</h3>
+    <p>
+        Here I've published my solutions to the exercises of Tarski's <i>Introduction to Logic</i>. Of course, any corrections or other correspondence on the subject is very welcome.
+    </p>
+    <p>
+        Right now, I've completed and published my solutions for the first part of the book (Chapters 1-6). I will update with the second part when they are completed, although this will take a while, since I have put my study of mathematical logic on the backburner a little in favor of other studies. These notes might be occasionally revised and updated. The title page will show the date of the latest revision.
+    </p>
+    <p>
+        The pdfs for each chapter are accessible here:
+    </p>
+    <ul>
+        <li><a href="../../../ex1.pdf">Chapter 1</a></li>
+        <li><a href="../../../ex2.pdf">Chapter 2</a></li>
+        <li><a href="../../../ex3.pdf">Chapter 3</a></li>
+        <li><a href="../../../ex4.pdf">Chapter 4</a></li>
+        <li><a href="../../../ex5.pdf">Chapter 5</a></li>
+        <li><a href="../../../ex6.pdf">Chapter 6</a></li>
+    </ul>
+</div>
+
+<style>
+    h3 {
+        text-align: center;
+    }
+    p { 
+        text-indent: 1rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+
+    .blog {
+        padding-bottom: 1rem;
+    }
+
+      /* mobile */
+    @media (max-width: 500px) {
+        p {
+            padding: .5rem;
+        }
+    }
+</style>
