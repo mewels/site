63c36a0d4aaf259c2ef8deb1ef68f9e839c402c3
mara <mara@nekopath.fun>
2025-08-20 20:14:40 -0700

    new texto

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,46 @@
 </script>
 <div>
     <p>
-        bienvenidos a mi sitio web! soy mara. también puedes llamarme bagel. me interesa búdismo, lógico, correr, la música y español. leer es mi primero amor.
+        bienvenidos a mi sitio web! soy mara. también puedes llamarme bagel pero nadie haga esto. me interesa búdismo, lógico, correr, la música y español. leer es mi primero amor.
     </p>
 
     <p>
-        siempre estoy intentando a mejorar mi habilidad de español. si lo hablas, prefería que me dirijas en español en vez de inglés. también, si tiene sugerencias para libros, películas, etc. en español, por favor dámelos.
+        esto sitio sirve varios propósitos. para recabar enlaces a mis obras variadas y para albergar su mismo algunas de estes obras. también aquí se utiliza como un laboratorio de pruebas para mejorar mi nivel de programación. 
     </p>
 
     <p>
-        puedes escuchar a mi música <a href = "https://soundcloud.com/nekopath">acá</a>.
+        siempre estoy intentando a mejorar mi habilidad de español. ojalá que algun dia pueda mudarme a españa o otra país hispanohablante. si lo hablas español, prefería que me dirijas en español en vez de inglés. también, si tiene sugerencias para libros, películas, etc. en español, por favor dámelos.
     </p>
 
     <p>
-        soy miembro de la círculo doujin <a href = "https://maillard.love"> imo team</a>. 
+        soy música de varias maneras. principalmente toco la guítarra y hacer música electrónica. usualmente, cúales lanzo son las obras electrónicas, pero recientemente he lanzado un sencillo de mi proyecto rock, se llama dead nymphs. puedes escuchar a mi música a los enlaces siguentes:
+    </p>
+
+    <nav>
+        <ul>
+            <li>
+                <a href = "https://soundcloud.com/nekopath">soundcloud</a>
+            (obras electrónicas)
+            </li>
+            
+            <li>
+                <a href = "https://nekopath.bandcamp.com">bandcamp</a>
+            (no hay mucho aquí)
+            </li>
+
+            <li>
+                <a href = "https://deadnymphs.bandcamp.com">dead nymphs</a>
+            (música rock)
+            </li>
+        </ul>
+    </nav>
+
+    <p>
+        soy miembro de el círculo doujin <a href = "https://maillard.love"> imo team</a>. usualmente compongo música para nuestras proyectas, y a veces también ayudar con la edición y la escritura.  
     </p>
 
     <p>
-        si te gusta cocinar, debes mirar a <a href = "https://meshi.world">mi otro sitio web</a>.
+        si te gusta cocinar, debes ver a <a href = "https://meshi.world">meshi world</a>, un sitio web que programé hace pocos años. a veces lo se rompe, así espero que funcione para ti. por favor enterame si notes errores. 
     </p>
     <br>
     <p style="text-align:center">
@@ -35,5 +58,15 @@
     div {
         margin-left: 2rem;
         margin-right: 2rem;
+    }
+
+    nav {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    ul {
+        list-style-type: "> ";
     }
 </style>
