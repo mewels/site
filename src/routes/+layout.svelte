9958aa120e67f80cdc79c0872df2e26fb1dfe295
mara <mara@nekopath.fun>
2025-10-05 22:55:27 -0700

    damn i need some svelte:head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,14 @@
 
 </script>
 
-<head>
+<svelte:head>
 <link rel="webchain"
     href="https://webchain.milkmedicine.net/" />
   <link rel="webchain-nomination"
     href="https://painfeeledsubstation.web.fc2.com/" />
   <link rel="webchain-nomination"
     href="https://yogurt200.com/" />
-</head>
+</svelte:head>
 <div class = "container">
     <Sidebar />
     <main id = "main" class = "page-width page-padding">
